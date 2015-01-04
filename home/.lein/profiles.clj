6256b7f96bb80256da58c96d4e1b46c440d9197e
Mark Sikora <marknsikora@gmail.com>
2015-01-04 14:19:14 -0500

    Track leiningen profile

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-typed "0.3.5"]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.2.1"]]}}
