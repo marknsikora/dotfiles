e50393c107166de36cd1f4e76e70f2be8af5418c
Mark Sikora <marknsikora@gmail.com>
2015-02-13 14:42:23 -0500

    Lein plugin to find outdated dependencies

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [jonase/eastwood "0.2.1"]
+                  [lein-ancient "0.6.2"]
                   [lein-kibit "0.0.8"]
                   [lein-typed "0.3.5"]]}}
